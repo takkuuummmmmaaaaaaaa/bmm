417a5e93bc18e137f41fd85baabd2d858fb29f41
takuma kurosawa <takkuuummmmmaaaaaaaa@gmail.com>
2018-02-20 17:21:14 +0900

    random variables from mixture beta distribution

---
+++ rv_bmm.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.stats as stat
+from itertools import chain
+
+def rvs_bmm(a, b, n, p):
+    
+    m = stat.multinomial.rvs(n, p)
+    rvs = [list(stat.beta.rvs(i, j, size=k)) for i, j, k in zip(a, b, m)]
+    rv = np.array(list(chain.from_iterable(rvs))).reshape((n,1))
+
+    return rv
+
+a = np.array([1, 3, 0.5])
+b = np.array([3, 1, 0.5])
+n = 100
+p = np.array([0.3, 0.6, 0.1])
+
+rv = rvs_bmm(a, b, n, p)
+print(type(rv), rv.shape, rv)
